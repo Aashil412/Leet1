d699cf96369667032bed9862285e8c301fe24e0f
Aashil Ahmad <103612954+Aashil412@users.noreply.github.com>
2024-10-19 15:17:11 -0400

    Time: 99 ms (100%), Space: 19.6 MB (99.78%) - LeetHub

--- 0475-heaters/0475-heaters.py
+++ 0475-heaters/0475-heaters.py
@@ -2,16 +2,35 @@ class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         houses.sort()
         heaters.sort()
-        heaters = [float('-inf')] + heaters + [float('inf')]
-        # 1, 2, 3
-        # -inf, 2, inf
-        i = 1
+        
         radius = 0
         for house in houses:
-            while heaters[i] < house:
-                i += 1
-            min_distance = min(house - heaters[i-1], heaters[i] - house)
+            position_of_heater = self.binary_search(heaters, house)
+            
+            if position_of_heater == 0:
+                left_heater = float('inf')
+            else:
+                left_heater = house - heaters[position_of_heater - 1]
+
+            if position_of_heater == len(heaters):
+                right_heater = float('inf')
+            else:
+                right_heater = heaters[position_of_heater] - house 
+            
+            min_distance = min(left_heater, right_heater)
             radius = max(radius, min_distance)
         return radius
+        
+        
+    def binary_search(self, heaters, house): # calculate the first heater to the right or equal to the house
+        left, right = 0, len(heaters) - 1
+        while left<=right:
+            middle = (left + right) // 2
+            if heaters[middle] < house:
+                left = middle + 1
+            else:
+                right = middle - 1
+        return left 
+    
         
     
