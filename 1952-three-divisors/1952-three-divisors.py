2e912f13d96fb5bdaacd9ff5db47c974e92c68e8
Aashil Ahmad <103612954+Aashil412@users.noreply.github.com>
2024-10-21 17:57:53 -0400

    Time: 7 ms (97.64%), Space: 16.6 MB (16.08%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.py
+++ 1952-three-divisors/1952-three-divisors.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(n, 1, -1):
             if n % i == 0:
                 count += 1
-        if count == 3:
-            return True
-        return False
+        return count == 3
+            
             
