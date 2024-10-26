ec4484ff4f00115ae187a145fe9c2779b86ac58b
Aashil Ahmad <103612954+Aashil412@users.noreply.github.com>
2024-10-26 11:24:47 -0400

    Time: 3 ms (92.13%), Space: 16.9 MB (57.29%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,28 +1,24 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        """
-        create res array to return
-        have a stack to add the current step youre on
-        1) check to see if the opening parentheses == closed parentheses == n 
-        (meaning if n is 3 there will be 3 open 3 closed)
-        2) check to see if closed < open
-        (we can add a closed)
-        3) check to see if open < n
-        (max amount of open we can add) 
-        """
-        res = []
+        results = []
         stack = []
-        def backTrack(open,closed):
+        def backtracking(open, closed):
             if open == closed == n:
-                res.append("".join(stack))
+                results.append(''.join(stack))
                 return
+            
             if open < n:
-                stack.append("(")
-                backTrack(open+1,closed)
+                stack.append('(')
+                backtracking(open + 1, closed)
                 stack.pop()
+            
             if closed < open:
-                stack.append(")")
-                backTrack(open,closed+1)
+                stack.append(')')
+                backtracking(open, closed + 1)
                 stack.pop()
-        backTrack(0,0)
-        return res
+        
+        backtracking(0,0)
+        return results
+
+
+        
