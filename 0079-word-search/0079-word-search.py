c8de9c89f826899dbaccd237a459f74d73d04093
Aashil Ahmad <103612954+Aashil412@users.noreply.github.com>
2025-05-20 15:11:43 -0400

    Time: 4685 ms (37.71%), Space: 17.9 MB (64.92%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,27 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS = len(board)
-        COLS = len(board[0]) 
+        COLS = len(board[0])
 
         def dfs(row, col, index):
+
             if index == len(word):
                 return True
-            if row < 0 or row >= ROWS or col < 0 or col >= COLS or board[row][col] != word[index]:
-                return False
-        
-            directions = [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]
-            temp = board[row][col]
-            board[row][col] = '#'
 
-            for r, c in directions:
-                if dfs(r, c, index + 1):
-                    return True
-            board[row][col] = temp
+            if 0 <= row < len(board) and 0 <= col < len(board[0]) and board[row][col] == word[index] and (row,col) not in visited:
+                visited.add((row,col))
+
+                results = (dfs(row - 1, col, index + 1) or
+                dfs(row + 1, col, index + 1) or
+                dfs(row, col - 1, index + 1) or
+                dfs(row, col + 1, index + 1)) 
+
+                visited.remove((row,col))
+
+                return results
+
             return False
 
         for row in range(ROWS):
             for col in range(COLS):
-                if dfs(row, col, 0):
-                    return True
+                if board[row][col] == word[0]:
+                    visited = set()
+                    if dfs(row, col, 0):
+                        return True
+
         return False
-        
